f1796e1739826501fc15bc746704ab2c9777b7f7
Igor Khotin <khotin@gmx.com>
2017-09-17 01:27:56 +0300

    [!] js13k version + updated todo list for future functionality

--- todo.ml
+++ todo.ml
@@ -1,13 +1,32 @@
-> actor entity
-> hovercraft entity
-> camera bind
-> hover mechanics
-> city generator stub
-> texture generator
-> primitive building model generator
-> drone model generator
-> containers model generator
-> projectile model generator
-> text objects
-> billboard
+> fix unlimited shield bug
+> add wall/drone hit penalty
+> smarter demo action cam
+> different cam angles
+> left/right strafe moves
+> explosions
+> bot collision avoidance
+> bot pathfinding
+> bot exploration mode
+> bot avoiding fire
+> bot smarter targeting
+> danger indicator
+> nearest enemies compas
+> radar
+> laser overheat
+> proper collision physics
+> quick-start sheet
+> manual
+> update sfx
+> music
+> industrial textures
+V actor entity
+V hovercraft entity
+V camera bind
+V hover mechanics
+V city generator stub
+V texture generator
+V pod model
+V projectile model
+V building model
+V drone model
 
